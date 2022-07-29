a37084ca72bcb6cdfd98343a0446f3d86c59075a
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-07-29 21:41:18 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -33,13 +34,48 @@ class HomePage extends StatelessWidget {
           crossAxisSpacing: 16,
           mainAxisSpacing: 16,
           children: const [
-            Card(),
+            Card(
+              child: Chart1(),
+            ),
             Card(),
             Card(),
             Card(),
           ],
         );
       }),
+    );
+  }
+}
+
+class Chart1 extends StatelessWidget {
+  const Chart1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 18, top: 18, bottom: 18),
+      child: BarChart(
+        BarChartData(
+            barGroups: [
+              BarChartGroupData(x: 0, barRods: [BarChartRodData(toY: 20)]),
+              BarChartGroupData(x: 1, barRods: [BarChartRodData(toY: 40)]),
+              BarChartGroupData(x: 2, barRods: [BarChartRodData(toY: 30)]),
+              BarChartGroupData(x: 3, barRods: [BarChartRodData(toY: 60)]),
+              BarChartGroupData(x: 4, barRods: [BarChartRodData(toY: 75)]),
+              BarChartGroupData(x: 5, barRods: [BarChartRodData(toY: 35)]),
+              BarChartGroupData(x: 6, barRods: [BarChartRodData(toY: 42)]),
+              BarChartGroupData(x: 7, barRods: [BarChartRodData(toY: 33)]),
+              BarChartGroupData(x: 8, barRods: [BarChartRodData(toY: 60)]),
+              BarChartGroupData(x: 9, barRods: [BarChartRodData(toY: 90)]),
+              BarChartGroupData(x: 10, barRods: [BarChartRodData(toY: 86)]),
+              BarChartGroupData(x: 11, barRods: [BarChartRodData(toY: 120)]),
+            ],
+            titlesData: FlTitlesData(
+              rightTitles:
+                  AxisTitles(sideTitles: SideTitles(showTitles: false)),
+              topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+            )),
+      ),
     );
   }
 }
